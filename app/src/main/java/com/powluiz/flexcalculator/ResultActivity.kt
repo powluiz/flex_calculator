1681d449195a6acf51cd95e38793ccf29eac8513
Luiz Ricardo Brumati De Lima <luiz.1303@hotmail.com>
2025-03-28 00:26:31 -0300

    chore: update ResultActivity

--- app/src/main/java/com/powluiz/flexcalculator/ResultActivity.kt
+++ app/src/main/java/com/powluiz/flexcalculator/ResultActivity.kt
@@ -5,21 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.text.DecimalFormat
 
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var tvResult : TextView
+    private lateinit var tvFuel1 : TextView
+    private lateinit var tvFuel2 : TextView
     private lateinit var btnRollback : Button
 
-    private fun findBestOption(consumption1: Float, consumption2: Float, fuelPrice1: Float, fuelPrice2: Float): String {
-        if (consumption1 == 0f || consumption2 == 0f || fuelPrice1 == 0f || fuelPrice2 == 0f) {
-            return "Algo deu errado! Por favor, tente novamente."
-        }
+    private fun getFuelValue(consumption: Float, fuelPrice: Float): Float {
+        if (consumption == 0f || fuelPrice == 0f) {  return 0f }
+        return fuelPrice / consumption
+    }
 
-        val fuelCost1 = fuelPrice1 / consumption1
-        val fuelCost2 = fuelPrice2 / consumption2
-        val bestOption = if (fuelCost1 < fuelCost2) 1 else 2
-        return "O combustível $bestOption é a melhor opção!"
+    private fun getFuelText(value1: Float, value2: Float): String {
+        if (value1 == value2) {  return "As opções são equivalentes!" }
+
+        val bestOption = if (value1 < value2) "1" else "2"
+        return "O combustível $bestOption é a melhor opção."
     }
 
     private fun onClickRollback() {
@@ -33,14 +37,22 @@ class ResultActivity : AppCompatActivity() {
 
         btnRollback = findViewById(R.id.rollback_button)
         tvResult = findViewById(R.id.result_value)
+        tvFuel1 = findViewById(R.id.fuel1_text)
+        tvFuel2 = findViewById(R.id.fuel2_text)
 
         btnRollback.setOnClickListener{onClickRollback()}
-
 
         val consumption1 = intent.getStringExtra("consumption1")?.toFloatOrNull() ?: 0f
         val consumption2 = intent.getStringExtra("consumption2")?.toFloatOrNull() ?: 0f
         val fuelPrice1 = intent.getStringExtra("fuelPrice1")?.toFloatOrNull() ?: 0f
         val fuelPrice2 = intent.getStringExtra("fuelPrice2")?.toFloatOrNull() ?: 0f
-        tvResult.text = findBestOption(consumption1, consumption2, fuelPrice1, fuelPrice2)
+
+        val fuelValue1 = getFuelValue(consumption1, fuelPrice1)
+        val fuelValue2 = getFuelValue(consumption2, fuelPrice2)
+
+        val df = DecimalFormat("0.00")
+        tvFuel1.text = "Gasto do combustível 1: ${df.format(fuelValue1)} R$ por Km"
+        tvFuel2.text = "Gasto do combustível 2: ${df.format(fuelValue2)} R$ por Km"
+        tvResult.text = getFuelText(fuelValue1, fuelValue2)
     }
 }
